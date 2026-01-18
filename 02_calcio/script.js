f3701660c04552e22722dda5bbe309449c4ad6ef
MattiaGalasso <galasso.mattia94@gmail.com>
2026-01-18 17:11:16 +0100

    Added Second Exercise

--- 02_calcio/script.js
+++ 02_calcio/script.js
@@ -7,3 +7,53 @@
     Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
     Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+//? General Variable
+const minPoint = 1;
+const maxPoint = 100;
+const minFoulsSuffered = 1;
+const maxFoulsSuffered = 500;
+
+const teams = [
+  {
+    name: "Milan",
+    point: 0,
+    foulsSuffered: 0,
+  },
+  {
+    name: "Inter",
+    point: 0,
+    foulsSuffered: 0,
+  },
+  {
+    name: "Juventus",
+    point: 0,
+    foulsSuffered: 0,
+  },
+  {
+    name: "Roma",
+    point: 0,
+    foulsSuffered: 0,
+  },
+  {
+    name: "Napoli",
+    point: 0,
+    foulsSuffered: 0,
+  },
+];
+
+const compactTeams = [];
+
+for (const team of teams) {
+  team.point = Math.floor(Math.random() * (maxPoint - minPoint) + minPoint);
+  team.foulsSuffered = Math.floor(
+    Math.random() * (maxFoulsSuffered - minFoulsSuffered) + minFoulsSuffered,
+  );
+  compactTeams.push({
+    name: team.name,
+    foulsSuffered: team.foulsSuffered,
+  });
+}
+
+console.table(teams);
+console.table(compactTeams);
